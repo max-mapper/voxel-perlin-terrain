c6f78c2d7ad3c8b3f352a38b06772bd207b89d6d
Max Ogden <max@maxogden.com>
2013-06-06 23:23:28 -0700

    start updating to use new perlin stuff

--- demo.js
+++ demo.js
@@ -1,89 +1,32 @@
-var createGame = require('voxel-engine')
-var THREE = require('three')
-var perlin = require('voxel-perlin-terrain')
+var perlin = require('./')
+var textures = "http://commondatastorage.googleapis.com/voxeltextures/"
 
 var chunkSize = 32
-var chunkDistance = 2
 
-window.generator = perlin(chunkDistance, chunkSize)
-window.game = createGame({
-  generateVoxelChunk: generator,
-  texturePath: './textures/',
-  materials: ['grass', 'obsidian', 'dirt', 'whitewool', 'crate'],
-  cubeSize: 25,
-  chunkSize: chunkSize,
-  chunkDistance: chunkDistance,
-  startingPosition: [35, 1000, 35],
-  worldOrigin: [0,0,0],
-  controlOptions: {jump: 6}
+window.generator = perlin('foo', 0, 5)
+
+var game = require('voxel-hello-world')({
+  texturePath: textures,
+  playerSkin: textures + 'player.png',
+  generateChunks: false,
+  materialFlatColor: true,
+  chunkDistance: 2,
+  materials: ['#ffffff']
 })
 
-// point camera down initially
-game.controls.pitchObject.rotation.x = -1.5
-
-game.on('collision', function (item) {
-  game.removeItem(item)
+game.voxels.on('missingChunk', function(p) {
+  var voxels = generator(p, chunkSize)
+  var chunk = {
+    position: p,
+    dims: [chunkSize, chunkSize, chunkSize],
+    voxels: voxels
+  }
+  game.showChunk(chunk)
 })
 
-var currentMaterial = 1
 
-function createDebris (pos, value) {
-  var mesh = new THREE.Mesh(
-    new THREE.CubeGeometry(4, 4, 4),
-    game.material
-  )
-  mesh.geometry.faces.forEach(function (face) {
-    face.materialIndex = value - 1
-  })
-  mesh.translateX(pos.x)
-  mesh.translateY(pos.y)
-  mesh.translateZ(pos.z)
-  
-  return {
-    mesh: mesh,
-    size: 4,
-    collisionRadius: 22,
-    value: value
-  }
-}
+var fly = require('voxel-fly')
+var makeFly = fly(game)
+makeFly(game.controls.target()).startFlying()
 
-function explode (pos, value) {
-  if (!value) return
-  var item = createDebris(pos, value)
-  item.velocity = {
-    x: (Math.random() * 2 - 1) * 0.05,
-    y: (Math.random() * 2 - 1) * 0.05,
-    z: (Math.random() * 2 - 1) * 0.05,
-  }
-  game.addItem(item)
-  setTimeout(function (item) {
-    game.removeItem(item)
-  }, 15 * 1000 + Math.random() * 15 * 1000, item)
-}
-
-game.appendTo('#container')
-
-game.on('mousedown', function (pos) {
-  var cid = game.voxels.chunkAtPosition(pos)
-  var vid = game.voxels.voxelAtPosition(pos)
-  if (erase) {
-    explode(pos, game.getBlock(pos))
-    game.setBlock(pos, 0)
-  } else {
-    game.createBlock(pos, currentMaterial)
-  }
-})
-
-var erase = true
-window.addEventListener('keydown', function (ev) {
-  if (ev.keyCode === 'X'.charCodeAt(0)) {
-    erase = !erase
-  }
-  ctrlDown = ev.ctrlKey
-})
-
-function ctrlToggle (ev) { erase = !ev.ctrlKey }
-window.addEventListener('keyup', ctrlToggle)
-window.addEventListener('keydown', ctrlToggle)
-
-game.requestPointerLock('canvas')
+window.game = game
--- index.js
+++ index.js
@@ -1,43 +1,33 @@
-var Perlin = require('perlin')
-var voxel = require('voxel')
-var generator = new Perlin()
+var noise = require('perlin').noise
 
-module.exports = function generate(opts) {
-  if (!opts) opts = {}
-  var chunkDistance = opts.chunkDistance || 2
-  var chunkSize = opts.chunkSize || 32
-  var scaleFactor = opts.scaleFactor || chunkDistance * chunkSize
-  getMaterialIndex = opts.getMaterialIndex || getMaterialIndex
-  var width = chunkDistance * 2 * chunkSize
-  var noise = this.noise = new Int8Array(width * width)
-  var lowerLeft = [0, 0]
-  var upperRight = [width, width]
-  generator.generate( lowerLeft, upperRight, function ( point, value ) {
-    noise[point[0] + point[1]*width] = ~~scale(value, 0, 1, 0, scaleFactor)
-  })
-  
-  // todo: investigate using typed array 'views' to lower memory footprint
-  return function perlinChunk(l, h) {
-    var fromLow = chunkSize * -chunkDistance
-    var fromHigh = chunkSize * chunkDistance
-    var toLow = lowerLeft[0]
-    var toHigh = upperRight[0]
-    return voxel.generate(l, h, function(x, y, z, n) {
-      x = scale(x, fromLow, fromHigh, toLow, toHigh)
-      z = scale(z, fromLow, fromHigh, toLow, toHigh)
-      y = scale(y, fromLow, fromHigh, toLow, toHigh)
-      var val = noise[x + z*width]
-      return getMaterialIndex(val, x, y, z)
+module.exports = function(seed, floor, ceiling) {
+  floor = floor || 0
+  ceiling = ceiling || 20 // minecraft's limit
+  noise.seed(seed)
+  return function generateChunk(position, width) {
+    var startX = position[0] * width
+    var startY = position[1] * width
+    var startZ = position[2] * width
+    var chunk = new Int8Array(width * width * width)
+    pointsInside(startX, startZ, width, function(x, z) {
+      var n = noise.simplex2(x/width , z/width)
+      var y = ~~scale(n, -1, 1, floor, ceiling)
+      if (y === floor || startY < y && y < startY + width) {
+        var xidx = Math.abs((width + x % width) % width)
+        var yidx = Math.abs((width + y % width) % width)
+        var zidx = Math.abs((width + z % width) % width)
+        var idx = xidx + yidx * width + zidx * width * width
+        chunk[idx] = 1
+      }
     })
+    return chunk
   }
 }
 
-function getMaterialIndex(val, x, y, z) {
-  if (y > val) return 0
-  if (20 > val && val < 30) return 2
-  if (30 > val && val < 40) return 3
-  if (val > 50) return 4
-  return 1
+function pointsInside(startX, startY, width, func) {
+  for (var x = startX; x < startX + width; x++)
+    for (var y = startY; y < startY + width; y++)
+      func(x, y)
 }
 
 function scale( x, fromLow, fromHigh, toLow, toHigh ) {
